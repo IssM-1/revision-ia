f4bf7cdadb8303c395198baa2d202beaae3cdb4c
IssM-1 <2.0.mchiri@gmail.com>
2026-06-14 15:50:40 +0000

    fix supabase credentials

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // CONFIG SUPABASE
 
-const supabaseUrl = "https://mghceiojyyckvbjubztq.supabase.co/rest/v1/";
+const supabaseUrl = "https://mghceiojyyckvbjubztq.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1naGNlaW9qeXlja3ZianVienRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0MzczNTksImV4cCI6MjA5NzAxMzM1OX0._t0mUeZtZeRhmaXGhhQHqJKsINyNbBFkj-iU18snfQQ";
 
 const supabaseClient = supabase.createClient(
